819eb0af1a0d802511b243c99409a0e61537ebc1
wangning113 <wangning29@jd.com>
2018-05-11 19:18:34 +0800

    项目架构搭建

---
+++ src/main/java/cn/edu/lnpu/cnsweb/CnsWebApplication.java
@@ -0,0 +1,16 @@
+package cn.edu.lnpu.cnsweb;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@MapperScan("cn.edu.lnpu.cnsweb.web.mapper")
+@ImportResource("classpath:xml/spring-config.xml")
+public class CnsWebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CnsWebApplication.class, args);
+  }
+}
